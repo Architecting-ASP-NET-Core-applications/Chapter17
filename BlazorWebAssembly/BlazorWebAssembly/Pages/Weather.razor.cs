05bb279a9f6a9566648536b22beef1d2db0d9d49
BitawareUnleashed <dario.benevento@gmail.com>
2024-12-13 23:10:10 +0100

    First commit

---
+++ BlazorWebAssembly/BlazorWebAssembly/Pages/Weather.razor.cs
@@ -0,0 +1,15 @@
+﻿using BlazorWebAssembly.Models;
+using System.Net.Http.Json;
+
+namespace BlazorWebAssembly.Pages;
+
+public partial class Weather
+{
+    public string Name { get; set; }
+    private WeatherForecast[]? forecasts;
+
+    protected override async Task OnInitializedAsync()
+    {
+        forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+    }
+}
